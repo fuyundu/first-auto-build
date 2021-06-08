ffe52301dcf22b98bb4ef12f143ff99a8197b580
fuyun.du <fuyun.du@pactera.com>
2021-06-08 18:29:05 +0800

    add docker

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn clean
 mvn package -DskipTests
-docker rmi -f lgj/webdemo:1.0
+sudo sudodocker rmi -f lgj/webdemo:1.0
 mvn dockerfile:build
-docker images
+sudo docker images
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-docker ps -a
-docker stop webdemo-8452
-docker rm -f webdemo-8452
-docker run -d --name webdemo-8452  -p 8452:8452 lgj/webdemo:1.0
-docker ps -a
+sudo docker ps -a
+sudo docker stop webdemo-8452
+sudo docker rm -f webdemo-8452
+sudo docker run -d --name webdemo-8452  -p 8452:8452 lgj/webdemo:1.0
+sudo docker ps -a
